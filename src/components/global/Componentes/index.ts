3909e497f3fcb2c7b4b141fda69639df434a752e
miguelyanpo-dev <miguel.yanpo@gmail.com>
2025-12-15 09:14:02 -0500

    add-changes

--- src/components/global/Componentes/index.ts
+++ src/components/global/Componentes/index.ts
@@ -1,6 +1,7 @@
 export { default as MenuView } from "./MenuView"
 export { default as DescargarApp } from "./DescargarApp"
 export { default as HeroSesion } from "./HeroSesion.astro"
+export { default as TrustSection } from "./TrustSection.astro"
 
 
 
